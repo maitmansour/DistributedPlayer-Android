8c69059115263d0fa72876ddba797a91ed809605
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-05-13 20:29:22 +0200

    Functionnal JS/HTML Separation

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,46 +1,49 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
 
-    // deviceready Event Handler
+    // Wait for device API libraries to load
     //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    function onLoad() {
+        document.addEventListener("deviceready", onDeviceReady, false);
     }
-};
 
-app.initialize();
+    // device APIs are available
+    function onDeviceReady() {
+        // Handle results
+        function startRecognition(){
+            window.plugins.speechRecognition.startListening(function(result){
+        // Show results in the console
+        console.log(result);
+    }, function(err){
+        console.error(err);
+    }, {
+        language: "en-US",
+        showPopup: true
+    });
+        }
+
+// Verify if recognition is available
+window.plugins.speechRecognition.isRecognitionAvailable(function(available){
+    if(!available){
+        console.log("Sorry, not available");
+    }
+
+    // Check if has permission to use the microphone
+    window.plugins.speechRecognition.hasPermission(function (isGranted){
+        if(isGranted){
+            startRecognition();
+        }else{
+            // Request the permission
+            window.plugins.speechRecognition.requestPermission(function (){
+                // Request accepted, start recognition
+                startRecognition();
+            }, function (err){
+                console.log(err);
+            });
+        }
+    }, function(err){
+        console.log(err);
+    });
+}, function(err){
+    console.log(err);
+});
+
+}
